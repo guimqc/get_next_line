9bb7f36668dc9d20e8fd2bf99943a0f01861f106
Guillaume Vial <gvial@c3r7p6.42quebec.com>
2022-05-19 15:30:58 -0400

    txt files to test bonus part

--- main.c
+++ main.c
@@ -2,25 +2,8 @@
 
 int main(void)
 {
-	int		fd = open("txt", O_RDONLY);
 	char	*next_line;
-	// int		i;
-
-	// i  = 0;
-	// while (i++ < 10)
-	// {
-	// 	next_line = get_next_line(fd);
-	// 	printf("%s", next_line);
-	// 	free(next_line);
-	// }
-
-	next_line = get_next_line(fd);
-	printf("%s", next_line);
-	free(next_line);
-
-	next_line = get_next_line(fd);
-	printf("%s", next_line);
-	free(next_line);
+	int		fd = open("txt", O_RDONLY);
 
 	next_line = get_next_line(fd);
 	printf("%s", next_line);

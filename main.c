61a8753d12e2b499a439f15a025ec97f88c522ac
vial <guillaumevial15@gmail.com>
2022-05-22 22:18:20 -0400

    main_bonus

--- main.c
+++ main.c
@@ -1,31 +1,31 @@
-#include "get_next_line.h"
+// #include "get_next_line.h"
 
-int main(void)
-{
-	char	*next_line;
-	int		fd = open("txt", O_RDONLY);
+// int main(void)
+// {
+// 	char	*next_line;
+// 	int		fd = open("txt", O_RDONLY);
 
-	next_line = get_next_line(fd);
-	printf("%s", next_line);
-	free(next_line);
+// 	next_line = get_next_line(fd);
+// 	printf("%s", next_line);
+// 	free(next_line);
 
-	next_line = get_next_line(fd);
-	printf("%s", next_line);
-	free(next_line);
+// 	next_line = get_next_line(fd);
+// 	printf("%s", next_line);
+// 	free(next_line);
 
-	next_line = get_next_line(fd);
-	printf("%s", next_line);
-	free(next_line);
+// 	next_line = get_next_line(fd);
+// 	printf("%s", next_line);
+// 	free(next_line);
 
-	next_line = get_next_line(fd);
-	printf("%s", next_line);
-	free(next_line);
+// 	next_line = get_next_line(fd);
+// 	printf("%s", next_line);
+// 	free(next_line);
 
-	next_line = get_next_line(fd);
-	printf("%s", next_line);
-	free(next_line);
+// 	next_line = get_next_line(fd);
+// 	printf("%s", next_line);
+// 	free(next_line);
 
-	next_line = get_next_line(fd);
-	printf("%s", next_line);
-	free(next_line);
-}
+// 	next_line = get_next_line(fd);
+// 	printf("%s", next_line);
+// 	free(next_line);
+// }
